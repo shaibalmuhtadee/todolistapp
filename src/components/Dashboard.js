7eac87fd7c9eff190b451cf0b81aaa7b4cf54f9c
Shaibal Muhtadee <shai@Shaibals-Air.ht.home>
2022-09-14 23:53:32 -0400

    todo list functionality completed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react"
 import { Card, Button, Alert, Container, Row, Col } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import { TextField } from "@material-ui/core"
+import { db } from "../firebase"
+import firebase from "firebase"
+import TodoListItem from "./Todo"
 
 export default function Dashboard() {
   const [error, setError] = useState("")
@@ -11,6 +15,34 @@ export default function Dashboard() {
   useEffect(() => {
     setInterval(() => setDateState(new Date()), 30000);
   }, []);
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    getTodos();
+  }, []); // run only on first launch
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress
+        }))
+      );
+    });
+  }
+  
+  function addTodo(e) {
+    e.preventDefault();
+    
+    db.collection("todos").add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+
+    setTodoInput("");
+  }
 
   async function handleLogout() {
     setError("")
@@ -66,39 +98,34 @@ export default function Dashboard() {
           </Col>
         </Row>
         <Row>
-          <Col>
+          <Col sm={6}>
             <Card className="w-100 text-center my-2 border-0">
-              <h4>Enter task for Shai</h4>
-              <input placeholder="Enter"/>
-            </Card>
+              <h4>His</h4>
+              <Row className="mx-2 align-items-center">
+                <form className="w-100">
+                  <TextField 
+                    className="col-10 pr-2"
+                    id="standard-basic" 
+                    label="Write a Todo"
+                    value={todoInput}
+                    onChange={(e) => setTodoInput(e.target.value)}
+                  />
+                  <Button type="submit" onClick={addTodo} className="col-2">Add</Button>
+                </form>
+              </Row>
+              {todos.map((todo) => (
+                <TodoListItem 
+                  todo={todo.todo} 
+                  inprogress={todo.inprogress} 
+                  id={todo.id} />
+              ))}
+            </Card>  
           </Col>
-          <Col>
-            <Card className="w-100 text-center my-2 border-0">
-              <h4>Enter task for Dee</h4>
-              <input placeholder="Enter"/>
-            </Card>
+          <Col sm={6}>
+            <h1>yeet</h1>
           </Col>
         </Row>
       </Container>
     </>
   )
 }
-
-
-/*
-          <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Profile</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <strong>Email:</strong> {currentUser.email}
-              <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-                Update Profile
-              </Link>
-              <div className="w-100 text-center mt-3">
-                <Button className="btn-primary w-100 text-white" onClick={handleLogout}>
-                  Log Out
-                </Button>
-              </div>
-            </Card.Body>
-          </Card>
-*/
